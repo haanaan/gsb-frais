f1db422a6dbda49d6090ff6a647e283bf03fc75c
Hanan <hanan.ashrafi@lycee-faure.fr>
2024-03-12 15:14:01 +0100

    controller saisie fiche

--- src/Controller/SaisirFicheFraisController.php
+++ src/Controller/SaisirFicheFraisController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\FicheFrais;
+use App\Form\MonthSelectorFormType;
+use App\Form\SaisirFraisForfaitType;
+use App\Form\SaisirFraisHorsForfaitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class SaisirFicheFraisController extends AbstractController
 {
     #[Route('/saisirFicheFrais', name: 'app_saisir_fiche_frais')]
-    public function index(): Response
+    public function saisirFiche(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $formHorsForfait = $this->createForm(SaisirFraisHorsForfaitType::class);
+        $formForfait = $this->createForm(SaisirFraisForfaitType::class);
+
+        $formHorsForfait->handleRequest($request);
+        $formForfait->handleRequest($request);
+
         return $this->render('saisir_fiche_frais/index.html.twig', [
             'controller_name' => 'SaisirFicheFraisController',
+
         ]);
     }
 }
